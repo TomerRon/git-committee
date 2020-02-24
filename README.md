b63994322a6821973527be6a2b9b4a0e1bf076c5
Tomer Ron <tomer@erlybird.com>
2020-02-24 11:04:23 +0100

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,84 @@
+# git-committee
+
+:octocat: git-committee is a little script that lets you make bulk backdated git commits.
+
+## Usecases
+
+### Leaving an organization
+
+Many developers use their work email when making commits to their work repositories. However, when they leave their organization (and unlink their work email), they lose their entire commit history!
+
+I was facing the same issue when I was leaving one of my previous companies and realized I was about to lose more than 2,000 commits from the last 2 years. I didn't want my contribution graph to become mostly empty, so I created this script!
+
+Run this script before you leave your organization on Github, and then push the generated repo after you leave the organization.
+
+### Copying another account's commit history
+
+You can use the script to copy the contribution history of another account (whether your own, or someone else's).
+
+## Usage
+
+- Clone this repo and make the script executable
+
+```
+$ git clone https://github.com/TomerRon/git-committee
+$ cd git-committee
+$ chmod +x committee.sh
+```
+
+- Run the script (refer to examples)
+
+```
+$ ./committee.sh -u torvalds -s 2020-02-01 -e 2020-02-29
+```
+
+- Sanity checks - make sure everything is in order before we push it
+
+```
+$ cd repos/<the generated folder>
+$ git logs
+$ git config -l
+```
+
+- [Create a new (private) repository](https://github.com/new) on Github
+
+- Add the origin
+
+```
+$ git remote add origin <my remote>.git
+```
+
+- Push it!
+
+```
+$ git push -u origin master
+```
+
+## Options and examples
+
+You can also read this by running `./committee.sh --help`
+
+```
+git-committee usage:
+---
+
+-u | --username   Choose GitHub username to use as input
+-f | --file       Choose JSON file to use as input
+
+-s | --start      Set the start date
+-e | --end        Set the end date
+
+-h | --help       Show this help screen
+
+---
+
+Examples:
+
+Use a GitHub user as the input, with start date and end date:
+./committee.sh -u torvalds -s 2020-02-01 -e 2020-02-29
+
+Use a JSON file as the input:
+./committee.sh -f ~/Downloads/data.json
+
+---
+```
---
+++ committee.sh
@@ -0,0 +1,154 @@
+usage()
+{
+    echo "git-committee usage:"
+    echo "---"
+    echo
+    echo "-u | --username   Choose GitHub username to use as input"
+    echo "-f | --file       Choose JSON file to use as input"
+    echo
+    echo "-s | --start      Set the start date"
+    echo "-e | --end        Set the end date"
+    echo
+    echo "-h | --help       Show this help screen"
+    echo
+    echo "---"
+    echo
+    echo "Examples:"
+    echo
+    echo "Use a GitHub user as the input, with start date and end date:"
+    echo "./committee.sh -u torvalds -s 2020-02-01 -e 2020-02-29"
+    echo
+    echo "Use a JSON file as the input:"
+    echo "./committee.sh -f ~/Downloads/data.json"
+    echo
+    echo "---"
+}
+
+##################
+# Parse arguments
+##################
+
+username=
+file=
+start=
+end=
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -u | --username )
+          shift
+          username=$1
+          ;;
+        -f | --file )
+          shift
+          file=$1
+          ;;
+        -s | --start )
+          shift
+          start=$1
+          ;;
+        -e | --end )
+          shift
+          end=$1
+          ;;
+        -h | --help )
+          usage
+          exit
+          ;;
+    esac
+    shift
+done
+
+if [ ! -z "$file" ] && [ ! -z "$username" ];
+then
+  echo "Error: received both file and GitHub username."
+  exit
+elif [ -z "$file" ] && [ -z "$username" ];
+then
+  echo "Error: you must supply a file or GitHub username."
+  exit
+fi
+
+##################
+# Prepare the data
+##################
+
+select=
+
+if [ ! -z "$start" ] && [ ! -z "$end" ];
+then
+  select=" | select(.date >= \"$start\" and .date <= \"$end\")"
+elif [ ! -z "$start" ];
+then
+  select=" | select(.date >= \"$start\")"
+elif [ ! -z "$end" ];
+then
+  select=" | select(.date <= \"$end\")"
+fi
+
+data=
+
+if [ ! -z "$file" ];
+then
+  data=`jq -c ".contributions[]$select" $file`
+elif [ ! -z "$username" ];
+then
+  data=`curl -s https://github-contributions.now.sh/api/v1/$username | jq -c ".contributions[]$select"`
+fi
+
+##################
+# Initialize repo
+##################
+
+today="$(date +"%Y-%m-%d")"
+random_string="$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 4 ; echo '')"
+dir="$today-$random_string-commits"
+
+echo "Creating new folder $dir..."
+
+mkdir -p repos
+cd repos
+mkdir -p $dir
+cd $dir
+
+echo "Initializing new git repository..."
+
+git init
+touch git-committee.md
+
+##################
+# Make the commits
+##################
+
+for c in $data
+do
+  date=$(echo $c | jq '.date')
+  count=$(echo $c | jq '.count')
+
+  if [ "$count" -gt 0 ]; then
+    echo '==='
+    echo $count commits on $date
+
+    export GIT_COMMITER_DATE="$date 12:00:00"
+    export GIT_AUTHOR_DATE="$date 12:00:00"
+
+    for i in `seq 1 $count`
+    do
+      echo "$date $i" > git-committee.md
+      git add git-committee.md
+      git commit --date="$date 12:00:00" -m "commit"
+    done
+  fi
+done
+
+echo '==='
+echo "All done!"
+echo
+echo "Now cd into the new directory..."
+echo "cd repos/$dir"
+echo
+echo "Add a git remote..."
+echo "git remote add origin <my remote>.git"
+echo
+echo "And push it!"
+echo "git push -u origin master"
